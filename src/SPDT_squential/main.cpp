73b8ef45feb70c33ff05e34e3ea2767d2667acb0
linna1998 <1500012782@pku.edu.cn>
2019-11-09 11:31:30 -0500

    Refine the parser. Fix some bugs in parsing. Add train function.

--- src/SPDT_squential/main.cpp
+++ src/SPDT_squential/main.cpp
@@ -5,15 +5,18 @@ int main() {
     DecisionTree decisionTree(256, 8, 128);
 
     Dataset trainDataset(2, 1605, 123);
-    Dataset testDataset(2, 30956, 123);
-
-	bool hasNext = true;
-
-    trainDataset.open_read_data("./data/a1a.train.txt");
+    Dataset testDataset(2, 30956, 123);	
+    
+    trainDataset.open_read_data("./data/a1a.train.txt");        
     decisionTree.train(trainDataset);
 
 	testDataset.open_read_data("./data/a1a.test.txt");	
     decisionTree.test(testDataset);
+
+    // DecisionTree decisionTree(256, 8, 128);
+    // Dataset trainDataset(2, 3, 30);
+    // trainDataset.open_read_data("./data/test.txt"); 
+    // decisionTree.train(trainDataset);           
 
     return 0;
 
