e83837f9d7a9cdbd9971ceb21d3983115dcf5b6b
adam <adam1679@163.com>
2019-11-22 22:09:21 -0500

    add py

---
+++ create_dataset.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+x, y = make_classification(n_samples=50000, 
+                           n_features=500, 
+                           n_informative=200)
+
+xt, xv, yt, yv = train_test_split(x, y, test_size=0.6)
+
+with open("data/generated.train.txt", "w") as f:
+    for i in range(xt.shape[0]):
+        x_line = xt[i, :].tolist()
+        x_line = ["{0}:{1:.6f}".format(j+1, value) for j, value in enumerate(x_line)]
+        line = str(yt[i]) + " " + " ".join(x_line) + "\n"
+        f.write(line)
+    
+with open("data/generated.test.txt", "w") as f:
+    for i in range(xv.shape[0]):
+        x_line = xv[i, :].tolist()
+        x_line = ["{0}:{1:.6f}".format(j+1, value) for j, value in enumerate(x_line)]
+        line = str(yv[i]) + " " + " ".join(x_line) + "\n"
+        f.write(line)
