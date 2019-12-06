99473aa40ec4326cf9115e420bc1438fad8a7a53
linna1998 <1500012782@pku.edu.cn>
2019-12-06 15:33:29 -0500

    Update the result of speedup table. Update the problems of evaluation into final report.md

--- Evaluation/evaluate.sh
+++ Evaluation/evaluate.sh
@@ -5,32 +5,32 @@ TARGETBIN_DATA="./decision-tree-data"
 TARGETBIN_DATA2="./decision-tree-data-openmp"
 TARGETBIN_CUDA="./decision-tree-cuda"
 
-DATASET="{0,1,5}"
+
 echo -e "Sequential mode"
-for i in $DATASET  
+for i in {0, 1, 5}  
 do  
 $TARGETBIN -i $i
 done  
 
-echo -e "Feature parallel"
+echo "Feature parallel"
 for i in {0, 1, 5}  
 do  
 $TARGETBIN_FEATURE -i $i
 done  
 
-echo -e "Data openmp"
+echo "Data openmp"
 for i in {0, 1, 5}  
 do  
 $TARGETBIN_DATA2 -i $i
 done  
 
-echo -e "Data openmpi"
+echo "Data openmpi"
 for i in {0, 1, 5}  
 do  
 $TARGETBIN_DATA -i $i
 done  
 
-echo -e "CUDA mode"
+echo "CUDA mode"
 for i in {0, 1, 5}  
 do  
 $TARGETBIN_CUDA -i $i
